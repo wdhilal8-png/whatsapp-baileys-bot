194c4001ab9f9d141a036fe54655e96687f38ea6
wdhilal8-png <wdhilal8@gmail.com>
2026-06-27 23:13:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys";
 
+const { state, saveCreds } = await useMultiFileAuthState("./session");
+
+const sock = makeWASocket({
+  auth: state,
+  printQRInTerminal: false
+});
+
+sock.ev.on("creds.update", saveCreds);
+
+const phone = "249125270800"; // اكتب رقمك بصيغة 249XXXXXXXXX
+
+if (!sock.authState.creds.registered) {
+  const code = await sock.requestPairingCode(phone);
+  console.log("Your Pairing Code:", code);
+}
