8e5cb6e2b37b6220eced423eab9a7d03ad683135
wdhilal8-png <wdhilal8@gmail.com>
2026-06-28 04:23:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import makeWASocket, {
   useMultiFileAuthState,
   DisconnectReason
 } from "@whiskeysockets/baileys";
+
 import pino from "pino";
 
-const startBot = async () => {
+async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("./session");
 
   const sock = makeWASocket({
@@ -15,19 +16,33 @@ const startBot = async () => {
 
   sock.ev.on("creds.update", saveCreds);
 
-  sock.ev.on("connection.update", async ({ connection }) => {
+  sock.ev.on("connection.update", async ({ connection, lastDisconnect }) => {
     if (connection === "open") {
       console.log("✅ WhatsApp Connected");
+    }
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+
+      console.log("❌ Connection Closed");
+
+      if (shouldReconnect) {
+        console.log("🔄 Reconnecting...");
+        startBot();
+      }
     }
   });
 
   if (!sock.authState.creds.registered) {
-    const phone = "249125764580"; // ضع رقمك مع رمز السودان
+    const phone = "249125270800"; // اكتب رقمك بدون +
     const code = await sock.requestPairingCode(phone);
-    console.log("================================");
+
+    console.log("=================================");
     console.log("PAIRING CODE:", code);
-    console.log("================================");
+    console.log("=================================");
   }
-};
+}
 
 startBot();
