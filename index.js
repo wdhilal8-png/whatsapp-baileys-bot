1dd0eec845c4dd30f7805769ef485468230a7086
wdhilal8-png <wdhilal8@gmail.com>
2026-06-28 09:14:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ import P from "pino";
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.get("/", (req, res) => {
-  res.send("WhatsApp Pairing Bot Running");
+app.get("/", (_, res) => {
+  res.send("WhatsApp Bot Running");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
 
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("./session");
-
   const { version } = await fetchLatestBaileysVersion();
 
   const sock = makeWASocket({
@@ -27,48 +26,30 @@ async function startBot() {
     auth: state,
     logger: P({ level: "silent" }),
     browser: ["Ubuntu", "Chrome", "22.0.0"],
-    printQRInTerminal: false,
-    syncFullHistory: false,
-    markOnlineOnConnect: false,
-    generateHighQualityLinkPreview: true,
-    connectTimeoutMs: 60000,
-    defaultQueryTimeoutMs: 60000
-});
+    printQRInTerminal: false
+  });
 
   sock.ev.on("creds.update", saveCreds);
 
-  sock.ev.on("connection.update", async (update) => {
-    const { connection, lastDisconnect } = update;
+  sock.ev.on("connection.update", async ({ connection }) => {
+
+    if (connection === "connecting") {
+
+      const code = await sock.requestPairingCode("249125270800");
+
+      console.log("=================================");
+      console.log("PAIRING CODE:", code);
+      console.log("=================================");
+    }
+
+    if (connection === "close") {
+      startBot();
+    }
 
     if (connection === "open") {
       console.log("✅ WhatsApp Connected");
     }
-
-    if (connection === "close") {
-      const shouldReconnect =
-        lastDisconnect?.error?.output?.statusCode !==
-        DisconnectReason.loggedOut;
-
-      console.log("❌ Connection Closed");
-
-      if (shouldReconnect) {
-        startBot();
-      }
-    }
   });
-  if (!state.creds.registered) {
-    const phone = "249125270800"; // ضع رقمك بصيغة 249XXXXXXXXX
-
-    try {
-      const code = await sock.requestPairingCode(phone);
-
-      console.log("================================");
-      console.log("PAIRING CODE:", code);
-      console.log("================================");
-    } catch (err) {
-      console.error("Failed to get Pairing Code:", err);
-    }
-  }
 }
 
 startBot();
